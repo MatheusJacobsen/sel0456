decd9167057a15004ef212132764d06a31823598
Matheus Jacobsen <matheusjacobsen@usp.br>
2022-09-21 18:03:33 -0300

    Versão Simplificada - Sequência de Fibonacci

--- fibonacci_simple.c
+++ fibonacci_simple.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
 
 // Declaração de Variáveis
 
-int n, fib = 0, a = 1, b = 1;
+int n, fib = 0, a = 1, b = 1, i;
 
 // Inserção de numero pelo Usuario
 
-printf("Digite um número inteiro: \n");
+printf("Digite um numero inteiro: \n");
 scanf("%d", &n);
 
 // Função de Fibonacci
 
-for(i=0;i<=n,i++){
+for(i=0;i<=n;i++){
     if(i == 0 || i == 1)
        fib = 1;
     else{
@@ -21,12 +21,12 @@ for(i=0;i<=n,i++){
        a = b;
        b = fib;
     }
-    printf("A sequencia de Fibonacci com %d numeros: %d \n", i, fib);
+    printf("\n A sequencia de Fibonacci com %d numeros: %d \n", i, fib);
 }
 
 // Resultado Final
 
-    printf("A sequencia final de Fibonacci com %d numeros: %d \n", n, fib);
+    printf("\n A sequencia final de Fibonacci com %d numeros: %d \n", n, fib);
 
     return 0;
 }
