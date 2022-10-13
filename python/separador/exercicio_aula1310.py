59f2a8ccb4b2a007a3d9b27930fc4eaef9de939c
Matheus Jacobsen <matheus.jacobsen@usp.br>
2022-10-13 15:40:10 -0300

    Subindo pasta

---
+++ python/separador/exercicio_aula1310.py
@@ -0,0 +1,20 @@
+# Ler um arquivo com dados separados com ","
+# Gerar um novo arquivo com os dados separados por ";"
+
+#class Dados:
+
+#    def _init_(self, dado):
+#        self.dado = dado
+
+#    def setDado(self,dado):
+#        self.dado = dado
+
+#    def get.Dado(self):
+#        return self.dado
+
+
+arq = open('arquivo_dados', 'r')
+linhas = arq.readlines()
+for linha in linhas:
+    print(linha)
+
