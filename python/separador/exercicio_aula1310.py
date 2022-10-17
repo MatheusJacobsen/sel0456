ec14787e3da1d06d8afaa1a576be69f4fa36b366
Matheus Jacobsen <matheusjacobsen@usp.br>
2022-10-17 10:53:37 -0300

    Versão final

--- python/separador/exercicio_aula1310.py
+++ python/separador/exercicio_aula1310.py
@@ -1,20 +1,15 @@
 # Ler um arquivo com dados separados com ","
 # Gerar um novo arquivo com os dados separados por ";"
 
-#class Dados:
-
-#    def _init_(self, dado):
-#        self.dado = dado
-
-#    def setDado(self,dado):
-#        self.dado = dado
-
-#    def get.Dado(self):
-#        return self.dado
+search = ","
+replace = ";"
 
 
-arq = open('arquivo_dados', 'r')
-linhas = arq.readlines()
-for linha in linhas:
-    print(linha)
+with open(r"C:/Users/mathe/OneDrive/Documentos/USP/Desenvolvimento de Software/sel0456/python/separador/dados.txt", "r") as file:
+    data = file.read()
+    data = data.replace(search, replace)
 
+with open(r'C:/Users/mathe/OneDrive/Documentos/USP/Desenvolvimento de Software/sel0456/python/separador/dados_atualizados.txt', "w") as file:
+    file.write(data)
+
+print("Novo arquivo com texto substituido criado")
